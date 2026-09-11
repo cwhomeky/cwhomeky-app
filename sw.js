15fef8ce1c03798162f57c3874c1794a6e027013
PATRICK C. WASHINGTON <claudewashingtonre@gmail.com>
2026-09-11 17:25:34 -0400

    Add service worker for asset caching
    Implement service worker for caching assets.

--- sw.js
+++ sw.js
@@ -1,1 +1,43 @@
+const CACHE_NAME = 'cwhomeky-mrp-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './app.js',
+  './manifest.json',
+  './assets/mrp-banner.jpg'
+];
 
+// Install Event
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activate Event
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch Event
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
